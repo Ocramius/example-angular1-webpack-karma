163e9cb10991736329895ffc0ac3e1f80c1f115e
Marco Pivetta <ocramius@gmail.com>
2017-05-10 17:09:04 +0200

    Extracting built CSS to an external file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './app/main.js',
@@ -10,18 +11,11 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                    },
-                ],
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
             },
         ],
     },
+    plugins: [
+        new ExtractTextPlugin("app.css")
+    ]
 };
